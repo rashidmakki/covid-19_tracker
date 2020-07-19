937629f90bd3f4b62c9341f1a11b10ead62491fa
Rashid Makki <rashidmakkiha_cse18@its.edu.in>
2020-07-19 22:14:00 +0530

    heading changed

--- src/component/search-box/search-box.jsx
+++ src/component/search-box/search-box.jsx
@@ -5,7 +5,7 @@ import './search-box.css';
 
 export const SearchBox = ({ placeholder, handleChange ,countryName}) => (
 	<div className='search-box'>
-	<img src={logo} alt='logo' />
+	<img src={logo} alt='logo' className='image'/>
 	<div className="ui icon input">
 	<input type="text" placeholder={placeholder} onChange={handleChange} />
 	<i aria-hidden="true" className="search circular inverted link icon"></i>
